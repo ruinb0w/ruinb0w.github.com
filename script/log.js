7e8c89c19524144f5b07d2043f363d4e0272d837
ruinb0w <ruinb0w@zoho.com>
2017-06-11 11:53:51 +0000

    update

--- script/log.js
+++ script/log.js
@@ -1,9 +1,15 @@
 window.onload = init;
-window.onresize = init;
+window.onresize = rheight;
 
-function init() {
-    var container = document.getElementById("container");
+var container;
+
+function init(){
+    container = document.getElementById("container");
+    rheight();
+}
+
+function rheight() {
     var height = document.documentElement.clientHeight;
-    container.style.height = height - 160 + "px";
+    container.style.height = height - 155 + "px";
 }
 
